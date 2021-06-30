a08a37cd846b61f05af2db403c7ac29a862b5e8b
workdog001 <1210073405@qq.com>
2021-06-30 19:58:31 +0800

    Update demo1.java

--- src/demo1.java
+++ src/demo1.java
@@ -1,6 +1,8 @@
 public class demo1 {
 
     public static void main(String[] args) {
+        System.out.println("你不好啊");
         System.out.println("你真好啊");
+        System.out.println("你挺好啊");
     }
 }
